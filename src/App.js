fc3de6780ebc618ac1e64a18cdf37bd078e76e38
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2022-12-13 01:17:05 +0300

    learn components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+const name = "Asu";
+const surname = "Fiskin";
+const isLoggedIn = false;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {isLoggedIn
+          ? `Benim adım ${name}, soyadım ${surname}`
+          : "Giriş yapmadınız."}
+      </h1>
     </div>
   );
 }
